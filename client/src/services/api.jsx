c15810462c17bcc6c5c1bc73778a26f810c79364
Abubakar Shaikh <64248752+abubakar-shaikh-dev@users.noreply.github.com>
2023-06-09 17:29:10 +0530

    Update api.jsx

--- client/src/services/api.jsx
+++ client/src/services/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://127.0.0.1:8000/api";
+const BASE_URL = import.meta.env.VITE_API_URL
 
 export async function getWeather(data){
     const {location} = data;
